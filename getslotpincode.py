7023f01ebba2e0f038070b9af380bcc24d802853
amritamilan <98206104+amritamilan@users.noreply.github.com>
2022-01-22 15:48:27 +0530

    Update getslotpincode.py

--- getslotpincode.py
+++ getslotpincode.py
@@ -1,18 +1,16 @@
 #To get covid vaccination slots by pincode and date
 import requests
 import json
-
 def processapidatato_json(data):
     text = json.dumps(data, sort_keys=True, indent=4)
     print(text)
-
-togetdata = {
+ 
+parameters = {
     "pincode": 411033,
-    "date": "23-01-2022"
+    "date": "22-01-2022"
 }
+response = requests.get(
+    "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin", params=parameters)
 
-apidata = requests.get(
-    "https://cdn-api.co-vin.in/api/v2​/appointment​/sessions​/public​/findByPin",params=togetdata)
-
-processapidatato_json(apidata.json())
+processapidatato_json(response.json())
 
