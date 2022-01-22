44ab991f9050874cca31f118cee23f707902cbf8
amritamilan <98206104+amritamilan@users.noreply.github.com>
2022-01-23 01:47:47 +0530

    Update getslotpincode.py

--- getslotpincode.py
+++ getslotpincode.py
@@ -1,8 +1,9 @@
 #To get covid vaccination slots by using pincode and date 
-
 import requests
 import json
 from json2html import *
+import webbrowser
+import os
 
 #Function 1 - To process data received from api to json format
 def processapidatato_json(data):
@@ -11,8 +12,18 @@ def processapidatato_json(data):
 
 #Paramters to be passed to the API [pincode + date]
 parameters = {
-    "pincode": 560048,
+   "pincode": 560048,
     "date": "22-01-2022"
+}
+
+# input user questions
+print("Covid vaccination tracker by pincode")
+input_pincode = input("Enter pincode ")
+input_date = input("Enter date in DD-MM-YY format ")
+
+parameters = {
+   "pincode": input_pincode,
+    "date": input_date
 }
 
 #Get information from API to get vaccination slots based on pincode and date
@@ -23,29 +34,24 @@ jsontohtml = json2html.convert(slot_information)
 
 #HTML Format - Head + Body
 htmlstart = """
-<html><head><title>>Covd-19 Vaccination Slots Tracker</title><style>
+<html>
+<head>
+<title>>Covd-19 Vaccination Slots Tracker</title>
+<style>
 table {
   font-family: Arial, Helvetica, sans-serif;
   border-collapse: collapse;
   width: 100%;
 }
 
-<Please give in pincode>
-Pincode:
-<br> <br>
-
-Date:
-<input>
-<br> <br>
-
 td, th {
-  border: 1px solid #ddd;
+  border: 1px solid #ddddd;
   padding: 8px;
 }
 
 tr:nth-child(even){background-color: #66b2b2;}
 
-tr:hover {background-color: #ddd;}
+tr:hover {background-color: #ddddd;}
 
 th {
   padding-top: 12px;
@@ -54,7 +60,9 @@ th {
   background-color: #66b2b2;
   color: white;
 }
-</style></head><body>
+</style>
+</head>
+<body>
 <h1> COVID-19 VACCINATION SLOTS FINDER </h1>
 """
 
@@ -67,9 +75,15 @@ htmlend = """
 contenttowrite = htmlstart + jsontohtml + htmlend
 
 #Write the complete content to file
-writetofile = open("covid19_vaccine_available_slots.html", "w")
+outputfilename = "covid19_vaccine_available_slots.html"
+writetofile = open(outputfilename, "w")
 writetofile.write(contenttowrite)
 writetofile.close()
-writetofile.close()
 
-print ("SUCCESS: Data retrieved from API and written to HTML file successfully!")
+# to open up a browser
+
+browserfile = 'file://' + os.path.realpath(outputfilename)
+
+
+print ("SUCCESS: Data retrieved from API and written to HTML file " + outputfilename )
+webbrowser.open(browserfile)
