ee2e0fc385cd3afaed0e702649a5fafb11499dbc
arpitamilan <98206125+arpitamilan@users.noreply.github.com>
2022-01-22 20:19:22 +0530

    Edit's and further program written.

--- getslotpincode.py
+++ getslotpincode.py
@@ -12,20 +12,55 @@ parameters = {
     "pincode": 411033,
     "date": "22-01-2022"
 }
-# find slots for pin code 
+#Find slots for pin-code
 response = requests.get(
     "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin", params=parameters)
+
 slot_information = processapidatato_json(response.json())
 
-print(slot_information)
+jsontohtml = json2html.convert(slot_information)
 
-htmlout = jason2html.converter(slot_information)
+htmlstart = """
+<html><head><title>>Covd-19 Vaccination Slots Tracker</title><style>
+table {
+  font-family: Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
 
-print(htmlout)
+td, th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
 
-file1 = open("slot_info.html", "w")
+tr:nth-child(even){background-color: #66b2b2;}
 
-file1.write(htmlout)
+tr:hover {background-color: #ddd;}
+
+th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #66b2b2;
+  color: white;
+}
+</style></head><body>
+<h1> COVID-19 VACCINATION SLOTS FINDER </h1>
+"""
+
+htmlend = """
+</body></html>
+"""
+
+contenttowrite = htmlstart + jsontohtml + htmlend
+
+writetofile = open("slot_info.html", "w")
+writetofile.write(contenttowrite)
+writetofile.close()
+writetofile.close()
+
+print ("SUCCESS: Data retrieved from API and written to HTML file successfully!")
+
 
 
 
